91955189748273340720a0f5b7e041a6aa9c64f4
yocosaka <sakayoco55@gmail.com>
2021-04-06 01:20:01 +0900

    Update getListData to make it work simply to get Data from API and seprate setData function

--- src/helpers/getListData.js
+++ src/helpers/getListData.js
@@ -1,7 +1,6 @@
 import axios from 'axios';
-import { addMovie } from '../actions/movies';
 
-const getListDataFromAPI = async (query) => {
+const getListData = async (query) => {
   const data = await axios.get('http://www.omdbapi.com/', {
     params: {
       apikey: process.env.REACT_APP_OMDB_API_KEY,
@@ -9,22 +8,6 @@ const getListDataFromAPI = async (query) => {
     },
   });
   return data.data.Search;
-};
-
-const getListData = async (store, query) => {
-  const listData = await getListDataFromAPI(query);
-  const newData = await listData.map((movie) => ({
-    imdbID: movie.imdbID,
-    title: movie.Title,
-    genre: movie.Genre,
-    year: movie.Year,
-    plot: movie.Plot,
-    posterURL: movie.Poster,
-    director: movie.Director,
-    actors: movie.actors,
-  }));
-  newData.map((movie) => store.dispatch(addMovie(movie)));
-  //
 };
 
 export default getListData;
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import { Provider } from 'react-redux';
 import ReactDOM from 'react-dom';
 import configureStore from './store/configureStore';
 import AppRouter from './routers/AppRouter';
+import { addMovie } from './actions/movies';
 import './assets/styles/style.scss';
 
 import getListData from './helpers/getListData';
@@ -14,11 +15,32 @@ store.subscribe(() => {
   console.log('MoviesStore:', movies);
   // eslint-disable-next-line no-console
   console.log('FiltersStore:', filters);
-  getListData(store, filters.title);
   // eslint-disable-next-line no-unused-vars
   // const visibleExpenses = getVisibleExpenses(expenses, filters);
   // console.log(visibleExpenses);
 });
+
+const setDataToStore = (query) => {
+  getListData(query).then((data) => {
+    console.log(data);
+    data.forEach((movie) => {
+      store.dispatch(addMovie({
+        imdbID: movie.imdbID,
+        title: movie.Title,
+        genre: movie.Genre,
+        year: movie.Year,
+        plot: movie.Plot,
+        posterURL: movie.Poster,
+        director: movie.Director,
+        actors: movie.actors,
+      }));
+    });
+  }).catch((error) => {
+    console.log(error);
+  });
+};
+
+setDataToStore('harry');
 
 const jsx = (
   <Provider store={store}>

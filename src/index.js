1be847ca4031a6fdb0337ca292f94317acadbe62
yocosaka <sakayoco55@gmail.com>
2021-04-08 11:51:32 +0900

    Add initial search in index.js

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import ReactDOM from 'react-dom';
 import configureStore from './store/configureStore';
 import AppRouter from './routers/AppRouter';
 import './assets/styles/style.scss';
+import { setTitleFilter } from './actions/filters';
 
 const store = configureStore();
 
@@ -14,3 +15,4 @@ const jsx = (
 );
 
 ReactDOM.render(jsx, document.getElementById('root'));
+store.dispatch(setTitleFilter('harry'));

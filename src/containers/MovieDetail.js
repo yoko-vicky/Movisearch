132112913d0aa735fb377c6e5552000104064328
yocosaka <sakayoco55@gmail.com>
2021-09-15 10:15:59 +0900

    Refactor MovieDetail.js

--- src/containers/MovieDetail.js
+++ src/containers/MovieDetail.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
@@ -10,38 +10,10 @@ import noImg from '../assets/images/no-img.jpg';
 import { updateMovie } from '../actions/movies';
 import NotFound from '../components/NotFound';
 
-class MovieDetail extends React.Component {
-  constructor(props) {
-    super(props);
-    this.getMovie = this.getMovie.bind(this);
-    this.state = {
-      error: '',
-    };
-  }
+const MovieDetail = ({ movie, updateMovie }) => {
+  const [error, setError] = useState('');
 
-  componentDidMount() {
-    this.runGetMovie();
-  }
-
-  componentDidUpdate(prevProps, prevState) {
-    if (!prevState.error) {
-      this.runGetMovie();
-    }
-  }
-
-  runGetMovie = () => {
-    const { movie } = this.props;
-    const {
-      Plot, Director, Actors, Genre,
-    } = movie;
-    if (!Plot || !Director || !Actors || !Genre) {
-      this.getMovie();
-    }
-  }
-
-  getMovie = async () => {
-    const { movie } = this.props;
-    const { updateMovie } = this.props;
+  const getMovie = async () => {
     try {
       const movieData = await getMovieData(movie.imdbID);
       const {
@@ -54,32 +26,41 @@ class MovieDetail extends React.Component {
         Genre,
       });
     } catch {
-      this.setState(() => ({ error: 'Sorry, unable to fetch the data.' }));
+      setError('Sorry, unable to fetch the data.');
     }
   };
 
-  render() {
-    const { movie } = this.props;
+  const runGetMovie = () => {
     const {
-      Poster, imdbID, Title, Year, Plot, Director, Actors, Genre,
+      Plot, Director, Actors, Genre,
     } = movie;
-    const { error } = this.state;
+    if (!Plot || !Director || !Actors || !Genre) {
+      getMovie();
+    }
+  };
 
-    return (
-      <div className="container pdb">
-        {error && <p className="error-msg">{error}</p>}
-        {Title === undefined ? <NotFound /> : (
-          <div className="movie">
-            <div className="movie__image-wrap">
-              <img src={Poster === 'N/A' ? noImg : Poster} alt={Title || `Movie-${imdbID}`} className="movie__image" />
-            </div>
-            <div className="movie__title-wrap">
-              <h2 className="movie__title">{Title}</h2>
-              <div className="movie__year">{Year}</div>
-            </div>
-            <div className="movie__text-wrap">
-              {Plot && <p className="movie__plot movie__spec">{Plot}</p>}
-              {Director && (
+  useEffect(() => {
+    runGetMovie();
+  }, [movie]);
+
+  const {
+    Poster, imdbID, Title, Year, Plot, Director, Actors, Genre,
+  } = movie;
+  return (
+    <div className="container pdb">
+      {error && <p className="error-msg">{error}</p>}
+      {Title === undefined ? <NotFound /> : (
+        <div className="movie">
+          <div className="movie__image-wrap">
+            <img src={Poster === 'N/A' ? noImg : Poster} alt={Title || `Movie-${imdbID}`} className="movie__image" />
+          </div>
+          <div className="movie__title-wrap">
+            <h2 className="movie__title">{Title}</h2>
+            <div className="movie__year">{Year}</div>
+          </div>
+          <div className="movie__text-wrap">
+            {Plot && <p className="movie__plot movie__spec">{Plot}</p>}
+            {Director && (
               <div className="movie__director movie__spec">
                 <h3 className="movie__spec__title">
                   <BiCameraMovie />
@@ -87,8 +68,8 @@ class MovieDetail extends React.Component {
                 </h3>
                 <p className="movie__spec__text">{Director}</p>
               </div>
-              )}
-              {Actors && (
+            )}
+            {Actors && (
               <div className="movie__actors movie__spec">
                 <h3 className="movie__spec__title">
                   <SiPostwoman />
@@ -96,8 +77,8 @@ class MovieDetail extends React.Component {
                 </h3>
                 <p className="movie__spec__text">{Actors}</p>
               </div>
-              )}
-              {Genre && (
+            )}
+            {Genre && (
               <div className="movie__genre movie__spec">
                 <h3 className="movie__spec__title">
                   <MdLocalMovies />
@@ -105,15 +86,14 @@ class MovieDetail extends React.Component {
                 </h3>
                 <p className="movie__spec__text">{Genre}</p>
               </div>
-              )}
-            </div>
-            <Link to="/" className="btn movie__btn">Back to Home</Link>
+            )}
           </div>
-        )}
-      </div>
-    );
-  }
-}
+          <Link to="/" className="btn movie__btn">Back to Home</Link>
+        </div>
+      )}
+    </div>
+  );
+};
 
 const mapStateToProps = (state, props) => ({
   movie: state.movies.find((movie) => movie.imdbID === props.match.params.id),
